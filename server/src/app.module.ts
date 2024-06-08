aa1c9fd6fb3540ea0351407bce5eed356d42d22e
NguyenNha <nguyenngothanhnha2003kg@gmail.com>
2024-06-08 11:01:42 +0700

    add docker file

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -11,20 +11,46 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [UserModule, CategoryModule, AuthModule, TransactionModule, ConfigModule.forRoot({ isGlobal: true }),
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   // useFactory: (configService: ConfigService) => ({
+    //   //   type: 'postgres',
+    //   //   host: configService.get('DB_HOST'),
+    //   //   port: configService.get('DB_PORT'),
+    //   //   username: configService.get('DB_USER_PG'),
+    //   //   password: configService.get('DB_PASSWORD'),
+    //   //   database: configService.get('DB_NAME'),
+    //   //   synchronize: true,
+    //   //   entities: [__dirname + '/**/*.entity{.js, .ts}'],
+    //   // }),
+    //   // inject: [ConfigService]
+    //   useFactory: (configService: ConfigService) => ({
+    //     type: 'postgres',
+    //     host: 'localhost',
+    //     port: 5432,
+    //     username: 'postgres',
+    //     password: '123456',
+    //     database: 'budget',
+    //     synchronize: true,
+    //     entities: [__dirname + '/**/*.entity{.js, .ts}'],
+    //   }),
+    //   inject: [ConfigService]
+    // })
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
+      useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USER_PG'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
+        host: configService.get<string>('DB_HOST', 'localhost'),
+        port: configService.get<number>('DB_PORT', 5432),
+        username: configService.get<string>('DB_USER', 'postgres'),
+        password: configService.get<string>('DB_PASSWORD', '123456'),
+        database: configService.get<string>('DB_NAME', 'budget'),
         synchronize: true,
         entities: [__dirname + '/**/*.entity{.js, .ts}'],
       }),
       inject: [ConfigService]
     })
+
   ],
   controllers: [AppController],
   providers: [AppService],
